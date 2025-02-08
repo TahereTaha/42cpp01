af6e64df1a9c6ec299640fab9a9d453b4564667f
Taha Tahere <elberkani.taha@gmail.com>
2025-02-08 14:33:31 +0100

    improved the makefile

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: tatahere <tatahere@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 15:16:39 by tatahere          #+#    #+#             */
-/*   Updated: 2025/02/07 15:28:49 by tatahere         ###   ########.fr       */
+/*   Updated: 2025/02/08 14:31:21 by tatahere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,8 @@ Zombie::Zombie(std::string _name)
 }
 
 Zombie::~Zombie(void)
-{}
+{
+}
 
 void	Zombie::announce(void)
 {
